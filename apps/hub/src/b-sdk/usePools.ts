6052a4ca29e29f1c6c31d741d156457cdd199e86
Paul McInnis <paulmcinnis99@gmail.com>
2024-10-30 17:06:16 -0400

    <feature>: functional pool UI with token selection, the three pool types, duplicate detection, and liquidity input. TBD the actual pool creation part

--- apps/hub/src/b-sdk/usePools.ts
+++ apps/hub/src/b-sdk/usePools.ts
@@ -1,12 +1,17 @@
 // import { BalancerSDK } from "@balancer-labs/sdk";
+
+import { PoolWithMethods } from "@balancer-labs/sdk";
 import useSWR from "swr";
 
 import { balancerClient } from "./balancerClient";
 
 export const usePools = () => {
   return useSWR("pools", async () => {
-    const pools = await balancerClient.pools.all();
-    console.log("USEPOOLS", pools);
-    return pools;
+    try {
+      return (await balancerClient.pools.all()) as PoolWithMethods[];
+    } catch (error) {
+      console.error("USEPOOLSERROR", error);
+      throw error;
+    }
   });
 };
