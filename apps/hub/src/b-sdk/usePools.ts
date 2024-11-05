a280955b513aca1394cd410c5a99562a42ad5afd
Paul McInnis <paulmcinnis99@gmail.com>
2024-11-04 21:19:35 -0500

    <fix>: cleanup and properly handle the token inputs with weight inputs

--- apps/hub/src/b-sdk/usePools.ts
+++ apps/hub/src/b-sdk/usePools.ts
@@ -1,11 +1,10 @@
-// import { BalancerSDK } from "@balancer-labs/sdk";
-
 import { PoolWithMethods } from "@balancer-labs/sdk";
 import useSWR from "swr";
 
 import { balancerClient } from "./balancerClient";
 
 export const usePools = () => {
+  // TODO: we should use v3 SDK here instead for better typing on the returned pools
   return useSWR("pools", async () => {
     try {
       return (await balancerClient.pools.all()) as PoolWithMethods[];
